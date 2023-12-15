ad5d672caab0428b6a52c4d7a1abbd27ba3d4198
Allderfun1 <73606615+Allderfun1@users.noreply.github.com>
2023-12-15 19:13:25 +0300

    страница авторизации

---
+++ js/auth.js
@@ -0,0 +1,19 @@
+const eyeFunc = (eyeId, inputId) => {
+    for (i = 0; i < inputId.length; i++) {
+        input = $(`#${inputId[i]}`);
+        input.attr('type', (input.attr('type') === 'password') ? 'text' : 'password');
+    }
+    $(`#${eyeId}`).toggleClass('pass-close');
+}
+
+const switchForm = (form) => {
+    if (form !== 'reg' && form !== 'log') form = 'reg';
+    document.querySelector('.forms-container').scrollTo((form === 'log') ? 500 : 0, 0);
+    location.hash = form;
+}
+
+switchForm(location.hash.split('#')[1]);
+
+window.onhashchange = () => { 
+    switchForm(location.hash.split('#')[1]);
+}
