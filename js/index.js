1f6fb6b7c2bdf3090569c2c54a76a0662d469d39
Allderfun1 <73606615+Allderfun1@users.noreply.github.com>
2023-12-17 06:01:20 +0300

    добавил поиск

--- js/index.js
+++ js/index.js
@@ -1,6 +1,5 @@
 const swiper = new Swiper('.swiper1', {
     speed: 500,
-    loop: true,
     autoplay: {
         delay: 7000,
     },
@@ -18,12 +17,16 @@ const swiper2 = new Swiper(".swiper2", {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
     },
+    pagination: {
+        el: '.swiper2-pagination',
+        clickable: true,
+    },
     breakpoints: {
-        1280: { slidesPerView: 5 },
-        1050: { slidesPerView: 4 },
-        880: { slidesPerView: 3 },
-        700: { slidesPerView: 2 },
-        520: { slidesPerView: 1 },
+        1280: { slidesPerView: 6 },
+        1050: { slidesPerView: 5 },
+        880: { slidesPerView: 4 },
+        700: { slidesPerView: 3 },
+        520: { slidesPerView: 2 },
         0: { slidesPerView: 1 },
     }
 });
@@ -32,4 +35,28 @@ $('.category-card').on('click', function() {
     $('.category-title').text($(this).children('span').text());
     $('.category-card').removeClass('selected-category');
     $(this).addClass('selected-category');
+    /* 
+    
+    тут пропишешь запрос 
+
+    $('.category-card').children('span').text() // здесь текст запроса (должность, приязанная к кнопке, на которую нажали)
+    $('.wrap__dop_info--doctors_card').html( сюда вставишь ответ(карточки, как в html) );
+    */
+});
+
+$('.request__input').on({
+    focus: () => { if ($('.request__input').val() !== '') $('.results').addClass('show-results'); },
+    blur: () => { $('.results').removeClass('show-results'); },
+});
+
+$('.request__button').on('click', () => {
+    document.querySelector('.request__input').focus();
+    $('.request-now').text(`Результаты поиска по запросу: ${$('.request__input').val()}`);
+    /* 
+    
+    тут пропишешь запрос 
+
+    $('.request__input').val() // здесь текст запроса
+    $('.results-container').html( сюда вставишь ответ(карточки, как в html) );
+    */
 });
